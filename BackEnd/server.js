43816142cf9afba513ed42b9faaf0b8c667f1893
Patipan Roungsuawn <56648202+patipanice@users.noreply.github.com>
2021-05-16 00:37:05 +0700

    date bug

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,9 +2,14 @@ var express = require("express");
 var cors = require("cors");
 const bodyParser = require("body-parser");
 const editarrData = require("./editarrData");
-const {resultRound} = require("./resultRound");
-var { getStatus, updateStatus, insertCard, getCard } = require("./connectMongo");
-var {createFile} = require('./logFile')
+const { resultRound } = require("./resultRound");
+var {
+  getStatus,
+  updateStatus,
+  insertCard,
+  getCard,
+} = require("./connectMongo");
+var { createFile } = require("./logFile");
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,25 +22,40 @@ var gameStatus = {
   game_round: 1,
   trump: "None",
   first_direction: "South",
+  date_time: "2021-05-15T17:04:35.680+00:00",
 };
 
-createFile();
+//createFile();
+
+
+  //let today = new Date();
+  //console.log(today.toString());
+ // console.log(today.toLocaleString());
+
 //Get status form Mongo
 const getStatusHandler = () => {
   getStatus().then((res) => {
-    gameStatus = {
-      game_match: res.game_match,
-      game_round: res.game_round,
-      trump: res.trump,
-      first_direction: res.first_direction,
-    };
-    if (gameStatus.game_round === 13) {
-      let myqueryStatus = { _id: gameStatus.game_match };
-      let newvaluesStatus = { $set: { game_match: gameStatus.game_match + 1 } };
-      updateStatus(myqueryStatus, newvaluesStatus)
-        .then(console.log("Update status success"))
-        .catch((err) => console.log(err));
-    }
+    res
+      .sort({ _id: -1 })
+      .limit(1)
+      .toArray((err, docs) => {
+        gameStatus = {
+          game_match: docs[0].game_match,
+          game_round: docs[0].game_round,
+          trump: docs[0].trump,
+          first_direction: docs[0].first_direction,
+          date_time: docs[0].date_time,
+        };
+        if (gameStatus.game_round === 13) {
+          let myqueryStatus = { _id: gameStatus.game_match };
+          let newvaluesStatus = {
+            $set: { game_match: gameStatus.game_match + 1 },
+          };
+          updateStatus(myqueryStatus, newvaluesStatus)
+            .then(console.log("Update status success"))
+            .catch((err) => console.log(err));
+        }
+      });
   });
 };
 
@@ -64,12 +84,12 @@ app.get("/write/:data", (req, res) => {
     arrData.push(data);
     if (arrData.length === 4) {
       console.log("[Card_Data] : " + arrData);
+      console.log(gameStatus.first_direction);
       arrData = editarrData(arrData, gameStatus.first_direction);
       let [winRound, first_direciton] = resultRound(arrData);
       console.log(
         "WinRound : " + winRound + " First_Direction : " + first_direciton
       );
-      //saveCards.saveCards(arrData, winRound, first_direciton, gameStatus);
       let myquery = { _id: gameStatus.game_match };
       let newvalues = {
         $set: {
@@ -78,43 +98,55 @@ app.get("/write/:data", (req, res) => {
             `${winRound}_${first_direciton}`,
           ],
         },
-      }; 
+      };
       insertCard(gameStatus, arrData, myquery, newvalues)
-        .then((res,err) => {
+        .then((res, err) => {
           if (err) throw err;
           console.log("Update record_card completed");
         })
         .then(() => {
           //update status when finish round
+          let date = new Date(); //get local time
           let myqueryStatus = { _id: gameStatus.game_match };
-          let newvaluesStatus = {
+          var newvaluesStatus = {
             $set: {
               game_round: gameStatus.game_round + 1,
               first_direction: first_direciton,
-            },
+              start_date_time: date,
+            } 
           };
-          updateStatus(myqueryStatus, newvaluesStatus).then((res,err) => {
+          if(gameStatus.game_round === 12) {
+            console.log("inside")
+             newvaluesStatus = {
+              $set: {
+                game_round: gameStatus.game_round + 1,
+                first_direction: first_direciton,
+                end_date_time: date,
+              }
+            };
+          } 
+          updateStatus(myqueryStatus, newvaluesStatus).then((res, err) => {
             if (err) throw err;
             console.log("ีีUpdate status completed");
             arrData = []; //reset
-            getStatusHandler();
+            getStatusHandler(); 
           });
         });
     }
-  }
-});
+  } 
+}); 
 
 //post status form front
 app.post("/poststatus", (req, res) => {
   let trump = req.body.trump;
   let first_direction = req.body.first_direction;
   let mySQL = { _id: gameStatus.game_match };
-  let newSQL = { $set: { trump: trump, first_direction: first_direction },};
-  updateStatus(mySQL,newSQL).then(err=>{
-      if(err) throw(err)
-      arrData = []
-      console.log("Update status complete");
-  })
+  let newSQL = { $set: { trump: trump, first_direction: first_direction } };
+  updateStatus(mySQL, newSQL).then((err) => {
+    if (err) throw err;
+    arrData = [];
+    console.log("Update status complete");
+  });
 });
 //get card api
 app.get("/card", (req, res) => {
@@ -126,24 +158,24 @@ app.get("/status", (req, res) => {
   readStatus(res);
 });
 
-//read card 
+//read card
 async function readCard(res) {
-  await getCard().then(response=>{
-    response.toArray((err,docs)=>{
-        if(err) throw(err)
-            res.json(docs);
-            console.log("Read card passed");
-        });
+  await getCard().then((response) => {
+    response.toArray((err, docs) => {
+      if (err) throw err;
+      res.json(docs);
+      console.log("Read card passed");
+    });
   });
 }
 
 //read status
 async function readStatus(res) {
-   await getStatus().then(response=>{ 
-     response.toArray((err,docs)=>{
-       if(err) throw(err)
-       res.json(docs);
+  await getStatus().then((response) => {
+    response.toArray((err, docs) => {
+      if (err) throw err;
+      res.json(docs);
       console.log("Read status passed");
-     })    
+    });
   });
 }
