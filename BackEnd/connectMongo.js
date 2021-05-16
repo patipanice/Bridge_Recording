5f8764f7fce5e7de97753fc0e2294bb4dcbedd96
Papawin Sremuang <papawin1st@gmail.com>
2021-05-16 21:09:08 +0700

    test

--- BackEnd/connectMongo.js
+++ BackEnd/connectMongo.js
@@ -5,7 +5,6 @@ const URL = "mongodb://localhost:27017/ContractBridgeDB";
 const STATUS = "status";
 const CARD = "card";
 
-
 //Connect MongoDB
 const connectMongo = MongoClient.connect(URL, {
   useNewUrlParser: true,
@@ -24,50 +23,44 @@ const getCard = () =>
     return dbo.collection(CARD).find({});
   });
 
-const updateStatus = (query, newquery) =>
-  connectMongo.then((db) => {
+const updateStatus = async (query, newquery) =>
+  await connectMongo.then((db) => {
     let dbo = db.db("ContractBridgeDB");
     return dbo.collection(STATUS).updateOne(query, newquery);
   });
 
-<<<<<<< HEAD
-  const createStatus = () => {
-    
-  }
-
-const insertCard = (status,arrData,myquery,newvalues) =>
-=======
-const createStatus = (query) => 
-  connectMongo.then(db=>{
+const createStatus = (query) =>
+  connectMongo.then((db) => {
     let dbo = db.db("ContractBridgeDB");
     return dbo.collection(STATUS).insertOne(query);
   });
 
-
+const createCard = (query) =>
+  connectMongo.then((db) => {
+    let dbo = db.db("ContractBridgeDB");
+    return dbo.collection(CARD).insertOne(query);
+  });
 /*const insertCard = (myquery, newvalues) =>
->>>>>>> 085597c5823a43ec08e8aeb07bc9028d7c191c4a
   connectMongo.then((db) => {
     let dbo = db.db("ContractBridgeDB");
     return dbo.collection("card").updateOne(myquery, newvalues);
   });*/
 
-  const insertCard = (myquery, newvalues) =>
+const insertCard = (myquery, newquery) =>
   connectMongo.then((db) => {
     let dbo = db.db("ContractBridgeDB");
-    return dbo.collection(CARD).updateOne(myquery, newvalues);
+    return dbo.collection(CARD).updateOne(myquery, newquery);
   });
 
-
-  const resetMongoInit = async (_id) =>{
-   await connectMongo.then((db) => {
-      let dbo = db.db("ContractBridgeDB");
-      dbo.collection(STATUS).deleteOne({'_id':_id},(err)=>{
-        if(err) throw(err)
-        console.log(`Delete ${_id} document completed`)
-      });
+const resetMongoInit = async (_id) => {
+  await connectMongo.then((db) => {
+    let dbo = db.db("ContractBridgeDB");
+    dbo.collection(STATUS).deleteOne({ _id: _id }, (err) => {
+      if (err) throw err;
+      console.log(`Delete ${_id} document completed`);
     });
-  }
-
+  });
+};
 
 module.exports = {
   getStatus,
@@ -75,5 +68,6 @@ module.exports = {
   insertCard,
   getCard,
   createStatus,
-  resetMongoInit
+  resetMongoInit,
+  createCard,
 };
